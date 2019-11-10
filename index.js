80cf70e030b7c996dc161e6e701928846e3c97cc
Thierry PHILIPPE <thierryphilippe@Macbook-Pro-de-Thierry.local>
2019-11-10 23:56:07 +0100

    Starting V4 (wip)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // polyfill customEvent pour IE
 ;(function() {
-  if (typeof window.CustomEvent === "function") return false;
-  function CustomEvent ( event, params ) {
+  if (typeof window.CustomEvent === "function")
+    return false;
+
+  function CustomEvent (event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
     var evt = document.createEvent('CustomEvent');
     evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
@@ -16,12 +18,13 @@
   var throttle = function(type, name) {
     var running = false;
     var func = function() {
-      if (running) return;
+      if (running)
+        return;
 
       running = true;
-        window.requestAnimationFrame(function() {
-          window.dispatchEvent(new CustomEvent(name));
-          running = false;
+      window.requestAnimationFrame(function() {
+        window.dispatchEvent(new CustomEvent(name));
+        running = false;
       });
     };
     window.addEventListener(type, func);
@@ -30,66 +33,70 @@
   /* init - you can init any event */
   throttle("resize", "ab-resize");
   throttle("scroll", "ab-scroll");
-  throttle("mousemove", "ab-mousemove");
-  throttle("touchmove", "ab-touchmove");
+  throttle("keyup", "ab-keyup");
 })();
+
+// deep extend function
+var extend = function() {
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]'){
+    deep = arguments[0];
+    i++;
+  }
+
+  var merge = function(obj) {
+    for (var prop in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+        if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+          extended[prop] = extend(true, extended[prop], obj[prop]);
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  for (; i < length; i++) {
+    merge(arguments[i]);
+  }
+
+  return extended;
+};
+
+// test if a string is a JSON
+var isJson = function(str) {
+  try {
+    JSON.parse(str);
+  } catch(e) {
+    return false;
+  }
+  return true;
+};
 
 
 window.AB = {
-  // deep extend function
-  extend: function() {
-    var extended = {},
-        deep     = false,
-        i        = 0,
-        length   = arguments.length;
-
-    if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]'){
-      deep = arguments[0];
-      i++;
-    }
-
-    var merge = function(obj) {
-      for (var prop in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-          if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-            extended[prop] = window.AB.extend(true, extended[prop], obj[prop]);
-          } else {
-            extended[prop] = obj[prop];
-          }
-        }
-      }
-    };
-
-    for (; i < length; i++) {
-      merge(arguments[i]);
-    }
-
-    return extended;
-  },
-
-  // test if a string is a JSON
-  isJson: function(str) {
-    try {
-      JSON.parse(str);
-    } catch(e) {
-      return false;
-    }
-    return true;
-  },
-
   runUpdaters: function(plugin) {
-    if (window.AB.options[plugin]) {
-      window.AB.plugins[plugin](window.AB.options[plugin]);
+    if (this.options[plugin]) {
+      this.plugins[plugin](this.options[plugin]);
     } else {
       for(var options in AB.options){
-        if(window.AB.options.hasOwnProperty(options))
-          window.AB.plugins[options](window.AB.options[options]);
+        if(this.options.hasOwnProperty(options)) {
+          this.plugins[options](this.options[options]);
+        }
       }
     }
   },
-
   plugins: {},
   options: {}
 };
 
-module.exports = window.AB;
+
+module.exports = {
+  extend: extend,
+  isJson: isJson,
+  AB: window.AB
+};
